f39472a58785ffd2c88afd28d198082d05ebb40d
Stéphane Litou <st3phh@gmail.com>
2012-09-20 14:13:09 +0200

    Multi wizard on same page support. Update the demo with more exemples

--- demo/js/scripts.js
+++ demo/js/scripts.js
@@ -13,5 +13,36 @@ $(function() {
 		buttonsClass: 'btn',
 		submitButtonClass: 'btn btn-info'
 	});
-	//$('#pagerMe').easyWizard('goToStep', 3);
+
+	$('#myWizard2').easyWizard({
+		buttonsClass: 'btn',
+		submitButtonClass: 'btn btn-info',
+		before: function(wizardObj, currentStepObj, nextStepObj) {
+			alert('Hello, I\'am the before callback');
+		},
+		after: function(wizardObj, prevStepObj, currentStepObj) {
+			alert('Hello, I\'am the after callback');
+		},
+		beforeSubmit: function(wizardObj) {
+			alert('Hello, I\'am the beforeSubmit callback');
+		}
+	});
+
+	$('#myWizard3').easyWizard({
+		showSteps: false,
+		showButtons: false,
+		submitButton: false
+	});
+	$('#myWizard3Pager .previous a').bind('click', function(e) {
+		e.preventDefault();
+		$('#myWizard3').easyWizard('prevStep');
+	});
+	$('#myWizard3Pager .page a').bind('click', function(e) {
+		e.preventDefault();
+		$('#myWizard3').easyWizard('goToStep', $(this).attr('rel'));
+	});
+	$('#myWizard3Pager .next a').bind('click', function(e) {
+		e.preventDefault();
+		$('#myWizard3').easyWizard('nextStep');
+	});
 });
